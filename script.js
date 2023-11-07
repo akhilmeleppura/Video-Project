305e1bbd381fbb92872cdfde5f43e87e8f097645
Akhil S <akhilmeleppura@gmail.com>
2023-11-07 22:24:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const video = document.getElementById('video-background');
+const playPauseButton = document.getElementById('play-pause-button');
+
+playPauseButton.addEventListener('click', () => {
+  if (video.paused) {
+    video.play();
+    playPauseButton.textContent = 'Pause';
+  } else {
+    video.pause();
+    playPauseButton.textContent = 'Play';
+  }
+});
